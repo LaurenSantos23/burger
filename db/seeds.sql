1fcd5b0d26256b695a113ce3678ec1d6110302cf
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-03 18:41:13 -0400

    added schema.sql and seeds.sql, server.js

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- Creates new rows
+INSERT INTO burgers(burger_name)
+VALUES ("Bacon Cheeseburger");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Turkey Burger");
+
+INSERT INTO burgers (burger_name)
+VALUES ("Mozzarella Stick Topped Mushroom Burger");
