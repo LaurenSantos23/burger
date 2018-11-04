b5535e082a6d22543f8c2ccf9d3daae093b535d1
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-03 20:16:32 -0400

    psuedo coded lines for orm

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,5 @@
 var connection = require('./connection.js');
 
+//Create Methods (mySQL queries) that will execute the necessary mySQL commands in the controllers
+// These are methods you will need in order to retrieve and store data in your database
+
