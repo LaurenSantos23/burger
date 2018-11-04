6924864acb0753818a1f3d0a356fadfca7f13840
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-04 13:40:40 -0500

    added some lines to orm.js and burgers_controller.js

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,18 @@
+//Import the MySQL connection 
 var connection = require('./connection.js');
 
 //Create Methods (mySQL queries) that will execute the necessary mySQL commands in the controllers
 // These are methods you will need in order to retrieve and store data in your database
+selectAll(){
 
+}
+
+
+insertOne(){
+
+}
+
+updateOne(){
+
+}
+module.export = orm;
---
+++ controllers/burgers_controller.js
@@ -0,0 +1,15 @@
+//Dependencies
+var express = require('express');
+
+var router = express.Router();
+
+//Import the model burger.js for database 
+var burger = require("../models/burgers.js");
+
+// create routes and logic (get, post, put, update, delete)
+
+
+
+
+//export routes for server.js to use
+module.exports = router;
