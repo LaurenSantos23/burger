e4d5ea6d683a3cefe98d0c9afeeca2fd384d6f34
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-04 19:32:17 -0500

    .

--- config/connection.js
+++ config/connection.js
@@ -5,9 +5,9 @@ var connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: root,
-    password:"password1",
+    password:"password",
     database: "burgers_db",
-});
+})
 
 // Make connection
 connection.connect(function(err) {
