0358a3be439dc68b30fec13676aa76022986a986
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-06 17:41:52 -0500

    trying to figure out how to link up my heroku

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 // Set up MySQL connection
 var mysql = require("mysql");
 var connection;
-
-if (process.env.JAWSDB_URL) {
- connection = mysql.createConnection(process.env.JAWSDB_URL);
+// is this supposed to use the JAWSDB_URL????
+if (process.env."mysql://p8w0fjfywzq7ar4a:lx8oxm4x71rc7se4@pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g7bwn935rfakxbxo") {
+ connection = mysql.createConnection(process.env."mysql://p8w0fjfywzq7ar4a:lx8oxm4x71rc7se4@pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/g7bwn935rfakxbxo");
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
