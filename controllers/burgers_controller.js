e6db8dd0bf832aa78e53d6494707636468594860
LaurenSantos23 <Lauren.Santosuosso@yahoo.com>
2018-11-08 18:50:49 -0500

    cleaning up code

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,9 @@
 //Dependencies
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 //Import the model burger.js for database 
-var burger = require("../models/burgers.js");
+var burger = require("../models/burgers.js")
 
 // create routes and logic (get, post, put, update, delete)
 
